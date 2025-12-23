b6435ed135ab29d64f057295c50c83e9a25c7bdc
mshadow <lijiav587@gmail.com>
2025-12-24 00:28:10 +0800

    unit test

--- lorm_page_test.go
+++ lorm_page_test.go
@@ -3,7 +3,9 @@ package lorm
 import (
 	"context"
 	"testing"
+	"time"
 
+	"github.com/shopspring/decimal"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,7 +14,35 @@ func TestQueryModelPageBranches(t *testing.T) {
 	defer e.Close()
 	ctx := context.TODO()
 
-	_, _, err := Query[*Test](e).Page(ctx, 1, 0)
+	// Prepare test data
+	testTime, _ := time.ParseInLocation(time.DateTime, "2025-01-23 16:17:18", time.Local)
+	models := []*Test{
+		{
+			Int:       1,
+			Bool:      true,
+			Str:       "a",
+			Timestamp: testTime,
+			Datetime:  testTime,
+			Decimal:   decimal.NewFromFloat(1.10),
+			IntSlice:  []int{1, 2, 3},
+			Struct:    Sub{ID: 1, Name: "haha"},
+		},
+		{
+			Int:       2,
+			Bool:      false,
+			Str:       "b",
+			Timestamp: testTime,
+			Datetime:  testTime,
+			Decimal:   decimal.NewFromFloat(2.12),
+			IntSlice:  []int{1, 2, 3},
+			Struct:    Sub{ID: 1, Name: "haha"},
+		},
+	}
+	repo := NewTestRepository(e)
+	_, err := repo.InsertAll(ctx, models)
+	assert.Nil(t, err)
+
+	_, _, err = Query[*Test](e).Page(ctx, 1, 0)
 	assert.Error(t, err)
 
 	list, total, err := Query[*Test](e).Where("id < ?", 0).Page(ctx, 1, 10)
